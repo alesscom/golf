b90462478a4a08d03499987e23441599535ec71b
alesscom <90730443+alesscom@users.noreply.github.com>
2021-12-21 11:46:48 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-var canvas = new fabric.Canvas('myCanvas');
-posisionpelota_y =100
-posisionpelota_x =100
-posisionollo_y = 100
-posisionollo_x = 100
+var canvas = new fabric.Canvas('canvas');
+posisionpelota_y =0;
+posisionpelota_x =0;
+posisionollo_y = 400;
+posisionollo_x = 800;
 
 block_image_width = 5;
 block_image_height = 5;
@@ -11,10 +11,12 @@ block_image_height = 5;
 function load_img(){
 	fabric.Image.fromURL('golf-h.png', function(Img){
 		block_image_object=Img;
-		block_image_object.scaleToWidth(700);
-		block_image_object.scaleToHeight(510);
-		block_image_object.set({top:0,
-		left:0});
+		block_image_object.scaleToWidth(50);
+		block_image_object.scaleToHeight(50);
+		block_image_object.set({
+			top:posisionollo_y,
+			left:posisionollo_x
+		});
 		canvas.add(block_image_object);
 	});
 	new_image();
@@ -24,13 +26,12 @@ function new_image()
 {
 	fabric.Image.fromURL('ball.png', function(Img){
 		block_image_object=Img;
-		block_image_object.scaleToWidth(700);
-		block_image_object.scaleToHeight(510);
-		block_image_object.set({top:0,
-		left:0});
+		block_image_object.scaleToWidth(50);
+		block_image_object.scaleToHeight(50);
+		block_image_object.set({top:posisionpelota_y,
+		left:posisionpelota_x});
 		canvas.add(block_image_object);
 	});
-	new_image();
 }
 
 window.addEventListener("keydown", my_keydown);
@@ -43,10 +44,12 @@ function my_keydown(e)
 	Si las coordenadas coinciden con las de la imagen de la pelota, elimina la imagen de la pelota, 
 	mostrar "¡TERMINÓ EL JUEGO!" 
 	y haz que el borde del canvas sea 'rojo'.*/
-	if(posisionpelota == posisionollo){
-
-	}
-	
+	if((posisionpelota_y == posisionollo_y) &&(posisionpelota_x == posisionollo_x)){
+         canvas.remove(block_image_object);
+        console.log("¡lo lograste!");
+		document.getElementById("hd3").innerHTML="¡lo lograste!";
+	    document.getElementById("canvas").style.borderColor="red";
+		}
 	else{
 		if(keyPressed == '38')
 		{
@@ -72,33 +75,45 @@ function my_keydown(e)
 	
 	function up()
 	{
-		if(player_y >= 0){
-			player_y = player_y - block_image_height;
+		if(posisionpelota_y >= 5){
+			posisionpelota_y = posisionpelota_y - block_image_height;
 			console.log("altura del bloque es igual " + block_image_height);
-			console.log("cuando se presiona la flecha hacia arriba, x = " + player_x + " , y = " + player_y);
-			canvas.remove(player_object);
-			load_img();
+			console.log("cuando se presiona la flecha hacia arriba, x = " + posisionpelota_x + " , y = " + posisionpelota_y);
+			canvas.remove(block_image_object);
+			new_image();
 		}
 	}
 
 	function down()
 	{
-		 // Escribe el código para mover la pelota hacia abajo.
+		if(posisionpelota_y <= 450){
+			posisionpelota_y = posisionpelota_y + block_image_height;
+			console.log("altura del bloque es igual " + block_image_height);
+			console.log("cuando se presiona la flecha hacia down, x = " + posisionpelota_x + " , y = " + posisionpelota_y);
+			canvas.remove(block_image_object);
+			new_image();
+		}
 	}
 
 	function left()
 	{
-		if(ball_x >5)
-		{
-			// Escribe el código para mover la pelota hacia la izquierda.
-		}
+			if(posisionpelota_x > 5){
+				posisionpelota_x = posisionpelota_x - block_image_width;
+				console.log("altura del bloque es igual " + block_image_width);
+				console.log("cuando se presiona la flecha hacia left, x = " + posisionpelota_x + " , y = " + posisionpelota_y);
+				canvas.remove(block_image_object);
+				new_image();
+			}
 	}
 
 	function right()
 	{
-		if(ball_x <=1050)
-		{
-			// Escribe el código para mover la pelota hacia la derecha.
+		if(posisionpelota_x <= 1050){
+			posisionpelota_x = posisionpelota_x + block_image_width;
+			console.log("altura del bloque es igual " + block_image_width);
+			console.log("cuando se presiona la flecha hacia right, x = " + posisionpelota_x + " , y = " + posisionpelota_y);
+			canvas.remove(block_image_object);
+			new_image();
 		}
 	}
 	
